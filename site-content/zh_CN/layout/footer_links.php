35cff7da4c1cfc14e56ae279b52a5832e80552d5
admin <admin@admindeMacBook-Pro.local>
2019-09-04 21:16:13 +0800

    Copy source from en

---
+++ site-content/zh_CN/layout/footer_links.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) Enalean, 2012 - 2017. All Rights Reserved.
+ *
+ * This file is a part of Tuleap.
+ *
+ * Tuleap is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Tuleap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Tuleap. If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<h3 class="footer-section-title">Contact</h3>
+<p class="footer-paragraph">
+    <a class="footer-link" href="/contact.php"><i class="fa fa-fw fa-envelope"></i> Contact us</a>
+    <br>
+    <a class="footer-link" href="https://twitter.com/TuleapOpenALM" target="_blank" rel="noreferrer"><i class="fa fa-fw fa-twitter"></i> @TuleapOpenALM</a>
+    <br>
+    <a class="footer-link" href="https://www.linkedin.com/groups/Tuleap-Community-3962145" target="_blank" rel="noreferrer"><i class="fa fa-fw fa-linkedin"></i> Tuleap Community</a>
+</p>
